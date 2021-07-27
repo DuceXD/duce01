b5a1a4149bc8f003ade674abbf471f7fa23b412c
DuceXD <s6202041620142@email.kmutnb.ac.th>
2021-07-27 17:19:59 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/duce01/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.duce01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ dart/hello.dart
@@ -0,0 +1,6 @@
+void main(){
+    print("Songsang");
+    print("Kotchakeng");
+    print("My nick name is Duce");
+    print("I'm 20 y");
+}
